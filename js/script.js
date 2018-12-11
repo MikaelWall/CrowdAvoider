6ab27e55f0787cac4b9f77e43484aac2d5bdbc58
linaeb <lina@linabe.se>
2018-12-11 14:24:04 +0100

    grundsida

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $.ajax({
+            type:"GET",
+            url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=n3CEf5CvEDPHCiKWf9blSDSGfqmglhsS",
+            async:true,
+            dataType: "json",
+            success: function(json) {
+                        console.log(json);
+                        // Parse the response.
+                        // Do other things.
+                     },
+            error: function(xhr, status, err) {
+                        // This time, we do not end up here!
+                     }
+          });
+    });
+});
+
+
+//$(".text").html(data.quote); 
+  //              $(".author").html(data.author);
