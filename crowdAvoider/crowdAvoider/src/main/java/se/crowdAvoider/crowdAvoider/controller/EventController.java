29a1d51b2a07863d2d325871d3069759a0a2afab
Mikael7Wall <mikael.7.wall@gmail.com>
2018-12-12 14:12:56 +0100

    Lade till JPA dependencies samt startade med EventRepository

---
+++ crowdAvoider/crowdAvoider/src/main/java/se/crowdAvoider/crowdAvoider/controller/EventController.java
@@ -0,0 +1,4 @@
+package se.crowdAvoider.crowdAvoider.controller;
+
+public class EventController {
+}
--- crowdAvoider/crowdAvoider/src/main/java/se/crowdAvoider/crowdAvoider/repository/EventRepository.java
+++ crowdAvoider/crowdAvoider/src/main/java/se/crowdAvoider/crowdAvoider/repository/EventRepository.java
@@ -1,9 +1,12 @@
 package se.crowdAvoider.crowdAvoider.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import se.crowdAvoider.crowdAvoider.domain.Event;
 
 @Repository
-public interface EventRepository extends  {
+public interface EventRepository extends CrudRepository<Event, Integer> {
 
+    
 
 }
