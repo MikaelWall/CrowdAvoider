4b5d41f844df98a1adcfac20ad0fc79de4320611
Mikael7Wall <mikael.7.wall@gmail.com>
2018-12-12 11:35:03 +0100

    Lagt in sökfunktionen för datum

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
-    $(".readyBtn").click(function () {
-        
-       var city = $("#cityselect option:selected").text();
-       console.log(city);
-        
-        $.ajax({
-            type:"GET",
-            url:"https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey=n3CEf5CvEDPHCiKWf9blSDSGfqmglhsS&city=" + city,
-            async:true,
-            dataType: "json",
-            success: function(json) {
-                        console.log(json);
-                        // Parse the response.
-                        // Do other things.
-                     },
-            error: function(xhr, status, err) {
-                        // This time, we do not end up here!
-                     }
-          });
-    });
-});
 
+                $(".readyBtn").click(function () {
 
-//$(".text").html(data.quote); 
-  //              $(".author").html(data.author);
+                    var city = $("#cityselect option:selected").text();
+                    console.log(city);
+
+                    var date = $(".datumSelect").val();
+                    console.log(date);
+
+                    $.ajax({
+                        type: "GET",
+                        url: "https://app.ticketmaster.com/discovery/v2/events.json?size=100&apikey=n3CEf5CvEDPHCiKWf9blSDSGfqmglhsS&city=" + city, // + "&localDate=" + date,
+                        async: true,
+                        dataType: "json",
+                        success: function (json) {
+                            console.log(json);
+
+                            for (var i = 0; i < (json._embedded.events).length; i++) {
+
+                                if (date == (json._embedded.events[i].dates.start.localDate)) {
+                                    console.log("Hittade resultat")
+
+                                }
+                            }
+
+                        },
+                        error: function (xhr, status, err) {
+                        }
+                    });
+                });
+            });
